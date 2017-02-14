e7a49938befe99b0c1bb9f6513c3aa1589908b7b
jack-skelton <jack.s1995@live.com.au>
2017-02-15 04:46:47 +1100

    landing page backdrop added

---
+++ app/js/init.js
@@ -0,0 +1,11 @@
+$(function(){
+      $(".element").typed({
+        //strings: ["web developers.", "graphic designers.","solution architects."],
+        strings: ["modern <strong>Web</strong> and <strong>Graphic</strong> design.", "increase your <b>Digital</b> reach."],
+        typeSpeed: 30,
+        loop: true,
+        showCursor: true,
+        backDelay: 3000,
+        startDelay: 0
+      });
+    });
