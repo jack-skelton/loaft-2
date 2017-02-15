e07b6580380068cb4b8ffb56a604d702c4f055d3
jack-skelton <jack.s1995@live.com.au>
2017-02-15 17:45:07 +1100

    chained tpying animations

--- app/js/init.js
+++ app/js/init.js
@@ -1,24 +1,26 @@
-$(function(){
-      $(".element").typed({
-        //strings: ["web developers.", "graphic designers.","solution architects."],
-        strings: ["modern <strong>Web</strong> and <strong>Graphic</strong> design.", "digital specialists and growth hackers", ],
-        typeSpeed: 10,
-        loop: true,
-        showCursor: false,
-        backDelay: 2000,
-        startDelay: 2000
-      });
-    });
+
 
 $(function(){
       $(".dingo").typed({
         //strings: ["web developers.", "graphic designers.","solution architects."],
-        strings: ["loaft "],
+        strings: ["loaft -"],
         typeSpeed: 30,
         loop: false,
         showCursor: true,
         backDelay: 3000,
-        startDelay: 500
+        startDelay: 500,
+        callback: function(){
+          this.showCursor = false;
+          $( ".typed-cursor" ).remove();
+          $(".element").typed({
+            //strings: ["web developers.", "graphic designers.","solution architects."],
+            strings: ["modern <strong>Web</strong> and <strong>Graphic</strong> design.", "<strong>digital</strong> specialists and <strong>growth</strong> hackers", ],
+            typeSpeed: 10,
+            loop: true,
+            showCursor: true,
+            backDelay: 2000,
+            startDelay: 1000
+          })}
       });
     });
 
