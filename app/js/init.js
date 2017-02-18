d8ea9d5678123a5f6e7f3a892b5d74c2428bbd34
Karl <karl.l1@hotmail.com>
2017-02-19 00:35:55 +1100

    new logos

--- app/js/init.js
+++ app/js/init.js
@@ -4,15 +4,15 @@ $(function(){
         $(".element").typed({
         //strings: ["web developers.", "graphic designers.","solution architects."],
         //"modern <strong>web</strong> and <strong>fraphic</strong> design. "<strong>digital</strong> specialists and <strong>growth</strong> hackers"
-        strings: ["modern <strong>web</strong> and <strong>graphic</strong> design.", "<strong>digital</strong> specialists and <strong>growth</strong> hackers"],
+        strings: ["modern <strong>web</strong> and <strong>graphic</strong> design", "<strong>digital</strong> specialists and <strong>growth</strong> hackers", "a design studio called <strong>Loaft.</strong>"],
         typeSpeed: 30,
-        loop: true,
+        loop: false,
         showCursor: true,
-        backDelay: 3500,
-        startDelay: 0,
-        preStringTyped: function() {
+        backDelay: 2000,
+        startDelay: 0
+        /*preStringTyped: function() {
             $("#typer").toggleClass("typer-change")
-        }
+        }*/
     })
 });
 
